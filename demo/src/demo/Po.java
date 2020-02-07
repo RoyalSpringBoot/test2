6ed131a2f2784eea91c0de536ead6e4c0eff035f
于 <@DESKTOP-V14RM27>
2020-02-07 11:13:01 +0800

    demo文件

---
+++ demo/src/demo/Po.java
@@ -0,0 +1,20 @@
+package demo;
+
+import java.util.Random;
+
+public class Po {
+	public static void main(String[] args) {
+		Random random=new Random();
+
+		while(true) {
+			int a=random.nextInt(100);
+			int b=random.nextInt(100);
+			int c=100;
+
+			if (a+b==c) {
+				System.out.println("a"+a+",b"+b);
+			}
+		}
+	}
+
+}
